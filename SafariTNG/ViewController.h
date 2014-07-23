33149780889f7ec2af63257495060db4f718270b
Iván Mervich <imervich@gmail.com>
2014-07-23 10:18:08 -0500

    Initial Commit

---
+++ SafariTNG/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SafariTNG
+//
+//  Created by Iván Mervich on 7/23/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
